8921f98c73f554227c414c71353a30b512fc1a5a
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2023-07-13 19:59:46 +0100

    Refactor router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -23,18 +23,13 @@ const Router = ({
   }, [setPage, page]);
 
   switch (page) {
+    case Pages.HOME:
+      return <Home onProceed={onProceed} />;
     case Pages.CAPTCHA:
       return <Captcha onProceed={onProceed} />;
-    case Pages.SCENARIO_ONE:
-    case Pages.SCENARIO_TWO:
-    case Pages.SCENARIO_THREE:
-    case Pages.SCENARIO_FOUR:
-    case Pages.SCENARIO_FIVE:
-    case Pages.SCENARIO_SIX:
-    case Pages.SCENARIO_SEVEN:
-    case Pages.SCENARIO_EIGHT:
-    case Pages.SCENARIO_NINE:
-    case Pages.SCENARIO_TEN:
+    case Pages.RESULTS:
+      return <Results />;
+    default:
       return (
         <Scenario
           currentScenario={page}
@@ -42,10 +37,6 @@ const Router = ({
           goBack={goBack}
         />
       );
-    case Pages.RESULTS:
-      return <Results />;
-    default:
-      return <Home onProceed={onProceed} />;
   }
 };
 
