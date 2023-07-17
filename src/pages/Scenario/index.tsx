0190cca855f76dfc35f2f8537987ef7592df9926
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2023-07-17 20:22:25 +0100

    Add transition to questions

--- src/pages/Scenario/index.tsx
+++ src/pages/Scenario/index.tsx
@@ -1,5 +1,9 @@
-import { useContext } from 'react';
-import { AnswersContext } from '../../context/AnswersContext';
+import { useContext, useRef } from 'react';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import ArrowSvg from '../../assets/arrow-back.svg';
+import './index.css';
+import { Scenarios } from './types';
+import { useGetScenarioCopy } from './useGetScenarioCopy';
 import {
   BackArrow,
   BackButton,
@@ -8,9 +12,7 @@ import {
   ScenarioContainer,
   ScenarioTitle,
 } from './styles';
-import { Scenarios } from './types';
-import { useGetScenarioCopy } from './useGetScenarioCopy';
-import ArrowSvg from '../../assets/arrow-back.svg';
+import { AnswersContext } from '../../context/AnswersContext';
 import { ProgressBar } from './ProgressBar';
 
 export const Scenario = ({
@@ -22,40 +24,76 @@ export const Scenario = ({
   readonly onProceed: () => void;
   readonly goBack: () => void;
 }) => {
+  const sceanrioOneRef = useRef<HTMLDivElement>(null);
+  const scenarioTwoRef = useRef<HTMLDivElement>(null);
+  const scenarioThreeRef = useRef<HTMLDivElement>(null);
+  const scenarioFourRef = useRef<HTMLDivElement>(null);
+  const scenarioFiveRef = useRef<HTMLDivElement>(null);
+  const scenarioSixRef = useRef<HTMLDivElement>(null);
+  const scenarioSevenRef = useRef<HTMLDivElement>(null);
+  const scenarioEightRef = useRef<HTMLDivElement>(null);
+  const scenarioNineRef = useRef<HTMLDivElement>(null);
+  const scenarioTenRef = useRef<HTMLDivElement>(null);
+  const scenarioRefs = [
+    sceanrioOneRef,
+    scenarioTwoRef,
+    scenarioThreeRef,
+    scenarioFourRef,
+    scenarioFiveRef,
+    scenarioSixRef,
+    scenarioSevenRef,
+    scenarioEightRef,
+    scenarioNineRef,
+    scenarioTenRef,
+  ];
+  const nodeRef = scenarioRefs[currentScenario - 2];
+
+  const { title, menaceCta, notMenaceCta } =
+    useGetScenarioCopy(currentScenario);
+
   const { updateAnswers } = useContext(AnswersContext);
   const answerQuestion = (isMenace: boolean) => {
     const currentQuestionNumber = currentScenario - 1;
     updateAnswers(currentQuestionNumber, isMenace);
   };
-
-  const { title, menaceCta, notMenaceCta } =
-    useGetScenarioCopy(currentScenario);
-
   return (
     <ScenarioContainer data-testid={currentScenario}>
       <BackButton onClick={goBack}>
         <BackArrow src={ArrowSvg} alt="back arrow" />
       </BackButton>
       <ProgressBar currentScenario={currentScenario} />
-      <ScenarioTitle>{title}</ScenarioTitle>
-      <ButtonContainer>
-        <Button
-          onClick={() => {
-            answerQuestion(true);
-            onProceed();
+      <SwitchTransition mode="out-in">
+        <CSSTransition
+          key={currentScenario}
+          nodeRef={nodeRef}
+          addEndListener={(done: () => void) => {
+            nodeRef.current!.addEventListener('transitionend', done, false);
           }}
+          classNames="fade"
         >
-          {menaceCta}
-        </Button>
-        <Button
-          onClick={() => {
-            answerQuestion(false);
-            onProceed();
-          }}
-        >
-          {notMenaceCta}
-        </Button>
-      </ButtonContainer>
+          <div ref={nodeRef}>
+            <ScenarioTitle>{title}</ScenarioTitle>
+            <ButtonContainer>
+              <Button
+                onClick={() => {
+                  answerQuestion(true);
+                  onProceed();
+                }}
+              >
+                {menaceCta}
+              </Button>
+              <Button
+                onClick={() => {
+                  answerQuestion(false);
+                  onProceed();
+                }}
+              >
+                {notMenaceCta}
+              </Button>
+            </ButtonContainer>
+          </div>
+        </CSSTransition>
+      </SwitchTransition>
     </ScenarioContainer>
   );
 };
