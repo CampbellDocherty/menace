84ec92ce35da4caa586c9958b173185f0aeaa96c
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2023-07-28 12:29:57 +0100

    Font size reduction

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,24 +2,21 @@ import { styled } from 'styled-components';
 
 export const Title = styled.h1<{ readonly size?: number }>`
   font-family: 'RoyalAcidOutline';
-  font-size: ${({ size }) => (size ? `${size}px` : '75px')};
+  font-size: ${({ size }) => (size ? `${size}px` : '64px')};
   margin: 0;
   margin-bottom: 4px;
   color: white;
   width: 100%;
   text-align: center;
   letter-spacing: 2px;
-  text-shadow:
-    -1px 1px 1px #000,
-    1px 1px 1px #000,
-    1px -1px 0 #000,
+  text-shadow: -1px 1px 1px #000, 1px 1px 1px #000, 1px -1px 0 #000,
     -1px -1px 0 #000;
 `;
 
 export const Footer = styled.p`
   position: fixed;
   left: 50%;
-  bottom: 20px;
+  bottom: 5px;
   transform: translate(-50%, -50%);
   margin: 0 auto;
   font-size: 14px;
