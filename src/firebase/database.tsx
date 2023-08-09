9b6c994b31ece51a23d32d7faeda66a14aaf246e
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2023-08-09 18:00:03 +0100

    Add leaderboard from database

--- src/firebase/database.tsx
+++ src/firebase/database.tsx
@@ -1,15 +1,36 @@
 import { app } from './index';
-import { getDatabase, set, ref } from 'firebase/database';
+import { getDatabase, set, ref, get, child } from 'firebase/database';
 
 export const database = getDatabase(app);
 
-type UserDetails = {
+export type UserDetails = {
   name: string;
   result: number;
   multiplier: number;
   completed: number;
 };
 
-export const addUser = (id: string, details: UserDetails) => {
-  set(ref(database, 'users/' + id), details);
+export const addUser = async (id: string, details: UserDetails) => {
+  await set(ref(database, 'users/' + id), details);
+};
+
+export const getUsers = async () => {
+  const response = {
+    users: null,
+    isError: false,
+  };
+  const dbRef = ref(database);
+  await get(child(dbRef, 'users/'))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        response.users = snapshot.val();
+      } else {
+        response.users = null;
+      }
+    })
+    .catch(() => {
+      response.isError = true;
+    });
+
+  return response;
 };
--- src/pages/Leaderboard/index.tsx
+++ src/pages/Leaderboard/index.tsx
@@ -1,17 +1,45 @@
-import { useContext, useMemo } from 'react';
-import { AnswersContext } from '../../context/Answers/AnswersContext';
+import { useEffect, useMemo, useState } from 'react';
 import { format } from 'date-fns';
-import {
-  calculateMultiplier,
-  calculateResult,
-} from '../Results/calculateResult';
 import Devil from '../../assets/devil.png';
 import { Cell, Heading, Multiplier, Table } from './styles';
+import { UserDetails, getUsers } from '../../firebase/database';
+import { ErrorText } from '../Captcha/styles';
 
 export const Leaderboard = () => {
-  const { answers, name } = useContext(AnswersContext);
-  const result = useMemo(() => calculateResult(answers), [answers]);
-  const multiplier = useMemo(() => calculateMultiplier(answers), [answers]);
+  const [users, setUsers] = useState<Record<string, UserDetails> | null>(null);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const populateUsers = async () => {
+      const response = await getUsers();
+      if (response.isError) {
+        return setError('Oops! Something went wrong, try to refresh 🤷‍♂️');
+      }
+      if (!response.users) {
+        return setUsers(null);
+      }
+
+      setUsers(response.users);
+    };
+
+    populateUsers();
+  }, []);
+
+  const normalizedUsers = useMemo(() => {
+    if (!users) return null;
+    const keys = Object.keys(users);
+    const flattenedUsers = keys.map((id) => {
+      return { id, ...users[id] };
+    });
+    const sortedArray = flattenedUsers.slice().sort((a, b) => {
+      return b.result - a.result;
+    });
+    return sortedArray;
+  }, [users]);
+
+  if (error) {
+    return <ErrorText>{error}</ErrorText>;
+  }
 
   return (
     <Table>
@@ -25,18 +53,25 @@ export const Leaderboard = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <Cell>#1</Cell>
-          <Cell>{name}</Cell>
-          <Cell>{result}%</Cell>
-          <Cell>
-            <Multiplier>
-              <p>x{multiplier}</p>
-              <img src={Devil} alt="Abolaji as a devil" />
-            </Multiplier>
-          </Cell>
-          <Cell>{format(Date.now(), 'dd MMM yy')}</Cell>
-        </tr>
+        {normalizedUsers &&
+          normalizedUsers.map((user, index) => {
+            const rank = index + 1;
+            const { name, multiplier, result } = user;
+            return (
+              <tr key={user.id}>
+                <Cell>#{rank}</Cell>
+                <Cell>{name}</Cell>
+                <Cell>{result}%</Cell>
+                <Cell>
+                  <Multiplier>
+                    <p>x{multiplier}</p>
+                    <img src={Devil} alt="Abolaji as a devil" />
+                  </Multiplier>
+                </Cell>
+                <Cell>{format(Date.now(), 'dd MMM yy')}</Cell>
+              </tr>
+            );
+          })}
       </tbody>
     </Table>
   );
